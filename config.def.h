0f8a3b019977c1854b82eceb09484ba713396d0b
Ethan Hunt <ethanweihunt@gmail.com>
2022-10-14 08:09:40 -0700

    change colorscheme to catppuccin

--- config.def.h
+++ config.def.h
@@ -15,15 +15,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=13" };
 static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=15";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_base[]        = "#24273a";
+static const char col_text[]        = "#cad3f5";
+static const char col_mantle[]      = "#1e2030";
+static const char col_gray[]        = "#6e738d";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                 fg        bg          border      */
+	[SchemeNorm]   = { col_gray, col_mantle, col_mantle  },
+	[SchemeSel]    = { col_text, col_base,   col_base    },
+	[SchemeTitle]  = { col_text, col_mantle, col_mantle  },
 };
 
 /* tagging */
